a8b29ae919bb0171428c4dcfe2342028372d91bf
Ryan Skelton <ryanskelton2000@yahoo.com>
2023-09-12 15:19:03 +0100

    Create namespace for gui rendering.

--- graphquery/core/include/gui.hpp
+++ graphquery/core/include/gui.hpp
@@ -1,7 +1,15 @@
 #pragma once
 
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <GLFW/glfw3.h>
+
 namespace graphquery::gui
 {
+    void Initialise(const char* window_name);
+    void Run();
 
-
+    inline GLFWwindow* m_window = nullptr;
+    inline constexpr ImVec4 m_background = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 }
--- graphquery/core/src/gui.cpp
+++ graphquery/core/src/gui.cpp
@@ -1,1 +1,102 @@
 #include <gui.hpp>
+
+static void Clean_Up();
+static void On_Update();
+
+void graphquery::gui::Initialise(const char* window_name)
+{
+    glfwInit();
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    // Create window with graphics context
+    m_window = glfwCreateWindow(1280, 720, window_name, nullptr, nullptr);
+    glfwMakeContextCurrent(graphquery::gui::m_window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+    io.ConfigViewportsNoAutoMerge = true;
+    io.ConfigViewportsNoTaskBarIcon = true;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(graphquery::gui::m_window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+}
+
+void graphquery::gui::Run()
+{
+    while(glfwWindowShouldClose(graphquery::gui::m_window) == 0)
+    {
+        On_Update();
+    }
+    Clean_Up();
+}
+
+static void On_Update()
+{
+    glfwPollEvents();
+
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    // Show demo window
+    ImGui::ShowDemoWindow();
+
+    // Rendering
+    ImGui::Render();
+    int display_w, display_h;
+    glfwGetFramebufferSize(graphquery::gui::m_window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+
+    glClearColor(graphquery::gui::m_background.x * graphquery::gui::m_background.w,
+                 graphquery::gui::m_background.y * graphquery::gui::m_background.w,
+                 graphquery::gui::m_background.z * graphquery::gui::m_background.w,
+                 graphquery::gui::m_background.w);
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    // Update and Render additional Platform Windows
+    // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+    //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+    if (ImGui::GetIO().ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        GLFWwindow* backup_current_context = glfwGetCurrentContext();
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+        glfwMakeContextCurrent(backup_current_context);
+    }
+
+    glfwSwapBuffers(graphquery::gui::m_window);
+}
+
+static void Clean_Up()
+{
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(graphquery::gui::m_window);
+    glfwTerminate();
+}
